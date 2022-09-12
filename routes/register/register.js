9670c87ab5c7393c870832ce9e551e7c3e7d7eeb
Mayur Jogiya <tiger@Mayurs-MacBook-Pro.local>
2022-09-13 02:03:20 +0530

    changed button and other css

--- routes/register/register.js
+++ routes/register/register.js
@@ -26,36 +26,49 @@ con.connect(function(error){
       console.log('Connected!:)');
     }
   });
-
+  var Profilefor;
+  var firstName;
+  var lastName 
+  var gender;
+  var religionUser;
+  var mothertongUser ;
+  var email;
+  var mobile;
+  var dob ;
+  var city;
 router.post('/newuser', (req, res) => {
-        const Profilefor = req.body.Profilefor;
-        const firstName = req.body.firstName;
-        const lastName = req.body.lastName;
-        const gender = req.body.gender;
-        const religionUser = req.body.religionUser;
-        const mothertongUser = req.body.mothertongUser;
-        const email = req.body.email;
-        const mobile = req.body.mobile;
-        const dob = req.body.dob;
-        const city = req.body.city;
+        Profilefor = req.body.Profilefor;
+        firstName = req.body.firstName;
+        lastName = req.body.lastName;
+        gender = req.body.gender;
+        religionUser = req.body.religionUser;
+        mothertongUser = req.body.mothertongUser;
+        email = req.body.email;
+        mobile = req.body.mobile;
+        dob = req.body.dob;
+        city = req.body.city;
+})
+
+router.post('/userdata', (req, res) => {
+    const livewith = req.body.livewith;
+    const maritalStatus = req.body.maritalStatus;
+    const children = req.body.children;
+    const diet = req.body.diet;
+    const subCommunity = req.body.subCommunity;
+    const qualification = req.body.qualification;
+    const workwith = req.body.workwith;
+    const profession = req.body.profession;
+    const income = req.body.income;
+    const about = req.body.about;
+    console.log(email);
     con.query("INSERT INTO register (firstn, lastn, profilefor, gender, religion, mothertong, email, mobile, dob, city) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", 
     [firstName, lastName, Profilefor, gender, religionUser, mothertongUser, email, mobile, dob, city], (err, res) => {
         console.log(err);
     })
-})
-
-router.post('/userdata', (req, res) => {
-    const livewith = livewith;
-    const maritalStatus = maritalStatus;
-    const children = children;
-    const diet = diet;
-    const subCommunity = subCommunity;
-    const qualification = qualification;
-    const workwith = workwith;
-    const profession = profession;
-    const income = income;
-    const about = about;
-    con.query("INSERT INTO userdata (")
+    con.query("INSERT INTO userdata (email, mobile, livewith, maritalstatus, children, diet, subcommunity, qualification, workin, profession, income, about) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
+    [email, mobile, livewith, maritalStatus, children, diet, subCommunity, qualification, workwith, profession, income, about], (err, res) => { 
+      console.log(err);
+    })
 })
 
 module.exports=router
